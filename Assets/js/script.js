e2effdd0e3ee5b713af92a9ed7d27debed501a36
Stephen Garcia <garciastephen5280@gmail.com>
2023-07-20 22:47:58 -0600

    final push

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,3 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
@@ -9,66 +8,56 @@ function generatePassword() {
   var password = "";
   var usableCharacters = "";
 
-  var passwordLength = promt(
+  var passwordLength = prompt(
     "Please choose a password length betweeen 8 and 128 characters."
   );
 
   if (passwordLength >= 8 && passwordLength <= 128) {
     console.log(passwordLength);
+
   } else {
     alert(
       "Please select a password length betweeen 8 and 128 characters. Previous selected is invalid."
     );
     return "";
-  }
+  };
 
-  var numQuestion = confirm(
-    "Do you want to use a number between 0 and 9 for your password?"
-  );
-  var lowerQuestion = confirm(
-    "Do you want to use lowercase letters in your password"
-  );
-  var upperQuestion = confirm(
-    "Do you want to use uppercase letters for your password?"
-  );
-  var specialQuestion = confirm(
-    "Do you want to use special characters for your password?"
-  );
+  var numQuestion = confirm("Do you want to use a number between 0 and 9 for your password?");
+  var lowerQuestion = confirm("Do you want to use lowercase letters in your password");
+  var upperQuestion = confirm("Do you want to use uppercase letters for your password?");
+  var specialQuestion = confirm("Do you want to use special characters for your password?");
 
   if (numQuestion) {
-    usableCharacters += numbers;
+    usableCharacters += numbers
   }
 
   if (lowerQuestion) {
-    usableCharacters += lowerCase;
+    usableCharacters += lowerCase
   }
 
   if (upperQuestion) {
-    usableCharacters += upperCase;
+    usableCharacters += upperCase
   }
 
   if (specialQuestion) {
-    usableCharacters += specialCharacters;
+    usableCharacters += specialCharacters
   }
 
-  if (!numQuestion && !lowerQuestion && upperQuestion && specialQuestion) {
+  if (!numQuestion && !lowerQuestion && !upperQuestion && !specialQuestion) {
     return alert("You must select at least one character.");
-  }
+  };
 
   for (let i = 0; i < passwordLength; i++) {
     password +=
       usableCharacters[Math.floor(Math.random() * usableCharacters.length)];
   }
   return password;
-}
+};
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
